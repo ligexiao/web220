1e389822e4112f715c1575f04e30503254910b86
grissomshen(沈陈霄) <grissomshen@tencent.com>
2015-03-15 14:41:00 +0800

    up

--- js/page0.js
+++ js/page0.js
@@ -3,10 +3,14 @@ $("#logo").fadeIn(3000);
 
 $(document).ready(function() {
     setTimeout(function() {
-        $("#logo").fadeOut(3000);
+        $("#logo").fadeOut(3000,function() {
+            window.location.href='page1_bt.html';   
+        });
     },3000);
 });
 
 $(document).click(function() {
-    $("#logo").fadeOut(1000);
+    $("#logo").fadeOut(1000,function() {
+        window.location.href='page1_bt.html';   
+    });
 });
